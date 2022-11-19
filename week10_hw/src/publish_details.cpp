f50e6d3ad4fca46c5b5ec9ef4aa8aa1c8602e294
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-11-18 19:39:22 -0500

    Created parameters

--- week10_hw/src/publish_details.cpp
+++ week10_hw/src/publish_details.cpp
@@ -11,12 +11,16 @@ using namespace std::chrono_literals;
 class DetailsPublisher : public rclcpp::Node {
 public:
   DetailsPublisher() : Node("details_publisher") {
+    week10_hw::msg::Details temp_;
+    this->declare_parameter("Name_of_topic", "publish_details");
+    this->declare_parameter("publishing_speed", 500);
+
     message_.first_name = "Koundinya";
     message_.last_name = "Vinnakota";
     message_.age = 22;
 
     details_publisher_ =
-        this->create_publisher<week10_hw::msg::Details>("publish_details", 10);
+        this->create_publisher<week10_hw::msg::Details>(this->get_parameter("Name_of_topic").as_string(), 10);
 
     service_ = this->create_service<week10_hw::srv::Details>(
         "change_details",
@@ -24,7 +28,7 @@ public:
                   std::placeholders::_1, std::placeholders::_2));
 
     timer_ = this->create_wall_timer(
-        500ms, std::bind(&DetailsPublisher::timer_callback, this));
+        std::chrono::milliseconds(this->get_parameter("publishing_speed").as_int()), std::bind(&DetailsPublisher::timer_callback, this));
 
     if (rcutils_logging_set_logger_level(
             this->get_name(),
@@ -34,14 +38,14 @@ public:
       RCLCPP_INFO(this->get_logger(), "Started without DEBUG");
 
     RCLCPP_DEBUG(this->get_logger(), " Publisher node started ");
-    if (this->get_parameter("publish_interval").as_int() < 1000)
+    if (this->get_parameter("publishing_speed").as_int() < 1000)
       RCLCPP_ERROR(this->get_logger(), "The speed of publish is too high!");
-    if (this->get_parameter("publish_interval").as_int() < 500) {
+    if (this->get_parameter("publishing_speed").as_int() < 500) {
       RCLCPP_FATAL(this->get_logger(),
                    "The speed of publish is extremely high! Quiting.");
     }
   }
-}
+
 
 private :
     /**
@@ -88,8 +92,8 @@ rclcpp::Publisher<week10_hw::msg::Details>::SharedPtr details_publisher_;
 rclcpp::TimerBase::SharedPtr timer_;
 week10_hw::msg::Details message_; // Custom message to publish
 rclcpp::Service<week10_hw::srv::Details>::SharedPtr service_; // service pointer
-}
-;
+};
+
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
